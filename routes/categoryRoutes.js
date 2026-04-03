029f926bf43681701a636ab2687ec99ff6eb9ee8
AasthaShree13 <aastha.shree.it27@heritageit.edu.in>
2026-04-04 01:17:09 +0530

    added admin authorization in category routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,19 +1,40 @@
-const { createCategoryController, getAllCategoryController, updateCategoryController, deleteCategoryController } = require("../controllers/categoryController");
+const {
+  createCategoryController,
+  getAllCategoryController,
+  updateCategoryController,
+  deleteCategoryController,
+} = require("../controllers/categoryController");
+const adminMiddleware = require("../middleware/adminMiddleware");
 const authMiddleware = require("../middleware/authMiddleware");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //create category - post
-router.post('/create', authMiddleware, createCategoryController);
+router.post(
+  "/create",
+  authMiddleware,
+  adminMiddleware,
+  createCategoryController,
+);
 
 //get all - get
-router.get('/getAll', getAllCategoryController);
+router.get("/getAll", getAllCategoryController);
 
 //update category - put
-router.put('/update/:id', authMiddleware, updateCategoryController);
+router.put(
+  "/update/:id",
+  authMiddleware,
+  adminMiddleware,
+  updateCategoryController,
+);
 
 //delete ctegory - delete
-router.delete('/delete/:id', authMiddleware, deleteCategoryController);
+router.delete(
+  "/delete/:id",
+  authMiddleware,
+  adminMiddleware,
+  deleteCategoryController,
+);
 
 module.exports = router;
